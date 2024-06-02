728d2b5da6da5e6260882435a168b3cf578f78ec
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-02 23:34:59 +0200

    fix: rename usePostBuildCallback dir

--- packages/velvet_framework/lib/widgets/pre_renderer.dart
+++ packages/velvet_framework/lib/widgets/pre_renderer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:velvet_framework/hooks/use_post_build_callaback/use_post_build_callback.dart';
+import 'package:velvet_framework/hooks/use_post_build_callback/use_post_build_callback.dart';
 
 class PreRenderer extends HookConsumerWidget {
   PreRenderer({
