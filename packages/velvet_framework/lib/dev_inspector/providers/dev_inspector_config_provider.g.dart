5049ec379db0ed72a1f717674ee33552a2490be0
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-03 00:41:57 +0200

    chore: update .g files

--- packages/velvet_framework/lib/dev_inspector/providers/dev_inspector_config_provider.g.dart
+++ packages/velvet_framework/lib/dev_inspector/providers/dev_inspector_config_provider.g.dart
@@ -7,12 +7,12 @@ part of 'dev_inspector_config_provider.dart';
 // **************************************************************************
 
 String _$devInspectorConfigHash() =>
-    r'4345f488f5ec7dff39efca18618d433c7ecc671d';
+    r'6d3456876e2cf0d34e3d3e38445fc1a2770f5000';
 
 /// See also [devInspectorConfig].
 @ProviderFor(devInspectorConfig)
 final devInspectorConfigProvider =
-    AutoDisposeProvider<DevInspectorConfigContract>.internal(
+    Provider<DevInspectorConfigContract>.internal(
   devInspectorConfig,
   name: r'devInspectorConfigProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -22,7 +22,6 @@ final devInspectorConfigProvider =
   allTransitiveDependencies: const <ProviderOrFamily>{},
 );
 
-typedef DevInspectorConfigRef
-    = AutoDisposeProviderRef<DevInspectorConfigContract>;
+typedef DevInspectorConfigRef = ProviderRef<DevInspectorConfigContract>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- packages/velvet_framework/lib/translation/providers/translation_config_provider.g.dart
+++ packages/velvet_framework/lib/translation/providers/translation_config_provider.g.dart
@@ -6,12 +6,11 @@ part of 'translation_config_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$translationConfigHash() => r'570fe042ad8ac6857992bd66d3346ef1e339fd0f';
+String _$translationConfigHash() => r'2b22054501d6383ddc70b3553e210b1403eefd74';
 
 /// See also [translationConfig].
 @ProviderFor(translationConfig)
-final translationConfigProvider =
-    AutoDisposeProvider<TranslationConfigContract>.internal(
+final translationConfigProvider = Provider<TranslationConfigContract>.internal(
   translationConfig,
   name: r'translationConfigProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,7 +20,6 @@ final translationConfigProvider =
   allTransitiveDependencies: const <ProviderOrFamily>{},
 );
 
-typedef TranslationConfigRef
-    = AutoDisposeProviderRef<TranslationConfigContract>;
+typedef TranslationConfigRef = ProviderRef<TranslationConfigContract>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
