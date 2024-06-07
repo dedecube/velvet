6dc32712c2519842a470a9787d9b139938d4c4ec
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-07 11:40:02 +0200

    feat: add generator and builder for VelvetConfig

---
+++ packages/velvet_generator/lib/src/builders/config_builder.dart
@@ -0,0 +1,28 @@
+import 'package:build/build.dart';
+import 'package:merging_builder/merging_builder.dart';
+import 'package:velvet_generator/src/generators/config_gererator.dart';
+
+/// Defines a merging builder.
+/// Honours the options: `input_files`, `output_file`, `header`, `footer`,
+/// and `sort_assets` that can be set in `build.yaml`.
+Builder bootstrapAutoloaderBuilder(BuilderOptions options) {
+  final defaultOptions = BuilderOptions({
+    'input_files': 'lib/config/*.dart',
+    'output_file': 'lib/config.velvet.dart',
+    'header': ConfigAutoloaderGenerator.header,
+    'footer': ConfigAutoloaderGenerator.footer,
+    'sort_assets': true,
+  });
+
+  // Apply user set options.
+  options = defaultOptions.overrideWith(options);
+
+  return MergingBuilder<List<String>, LibDir>(
+    generator: ConfigAutoloaderGenerator(),
+    inputFiles: options.config['input_files'],
+    outputFile: options.config['output_file'],
+    header: options.config['header'],
+    footer: options.config['footer'],
+    sortAssets: options.config['sort_assets'],
+  );
+}
