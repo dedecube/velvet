f182bbc53caefef7bd569be6de3947b7d5815bec
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-12 10:58:51 +0200

    feat: add velvet_custom_lints package

---
+++ packages/velvet_custom_lints/README.md
@@ -0,0 +1,17 @@
+# Velvet Custom Lints
+
+This package provides custom lints for the Velvet framework.
+
+## Lints
+
+### `one_class_per_file`
+
+This lint enforces that there is only one class per file.
+
+## Contributing
+
+If you would like to contribute to Velvet, please read the [contributing guidelines](../../CONTRIBUTING.md) before submitting a pull request.
+
+## License
+
+Velvet is released under the [MIT License](LICENSE).
---
+++ packages/velvet_custom_lints/lib/velvet_custom_lints.dart
@@ -0,0 +1,18 @@
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:velvet_custom_lints/src/rules/one_class_per_file.dart';
+
+// Entrypoint of plugin
+PluginBase createPlugin() => _VelvetCustomLints();
+
+// The class listing all the [LintRule]s and [Assist]s defined by our plugin
+class _VelvetCustomLints extends PluginBase {
+  // Lint rules
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) => [
+        OneClassPerFile(),
+      ];
+
+  // Assists
+  @override
+  List<Assist> getAssists() => [];
+}
