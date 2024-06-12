b5479317e93edca764192067fff13dde3baac81b
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-12 11:03:09 +0200

    fix: fallback dioException message on HttpRequestException

--- packages/velvet_framework/lib/http/exceptions/http_request/http_request_exception.dart
+++ packages/velvet_framework/lib/http/exceptions/http_request/http_request_exception.dart
@@ -3,10 +3,10 @@ import 'package:velvet_framework/http/contracts/http_request_contract.dart';
 
 class HttpRequestException implements Exception {
   HttpRequestException({
-    required this.message,
     required this.httpRequest,
     required this.dioException,
-  });
+    String? message,
+  }) : message = message ?? dioException.message;
 
   final String? message;
   final HttpRequestContract httpRequest;
