433f33006579d5e05212f48bda9df33dc3282864
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-20 09:56:31 +0200

    feat: wrap build.yaml from vendor

--- packages/velvet_generator/lib/velvet_generator.dart
+++ packages/velvet_generator/lib/velvet_generator.dart
@@ -1,11 +1,5 @@
 library velvet_generator;
 
-/// Builders
-
+export 'src/builders/autoloader_builder.dart';
 export 'src/builders/bootstrap_builder.dart';
-export 'src/builders/config_builder.dart';
-
-/// Generators
-
 export 'src/generators/bootstrap_generator.dart';
-export 'src/generators/config_generator.dart';
