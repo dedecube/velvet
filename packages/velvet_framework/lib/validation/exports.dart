d383840051054d5da5e281986b33de66b9de679a
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-25 21:50:22 +0200

    fix: export also max_length_rule

--- packages/velvet_framework/lib/validation/exports.dart
+++ packages/velvet_framework/lib/validation/exports.dart
@@ -1,6 +1,7 @@
 // @index(['./**/(*.dart)','!./**/*.g.dart', '!./**/_*.dart'], f => `export '${f.path}.dart';`)
 export './rule.dart';
 export './rules/email_rule.dart';
+export './rules/max_length_rule.dart';
 export './rules/min_length_rule.dart';
 export './rules/required_string_rule.dart';
 export './validator.dart';
