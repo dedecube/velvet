da0b36546928d4be48fa7419bd9116332c7c816a
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-25 21:48:29 +0200

    docs: describe usePostBuildCallback

--- packages/velvet_framework/lib/hooks/use_post_build_callback/use_post_build_callback.dart
+++ packages/velvet_framework/lib/hooks/use_post_build_callback/use_post_build_callback.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+/// Executes a callback function after the widget has finished building.
+///
+/// This hook is useful when you need to perform an action after the widget has been fully built and rendered on the screen.
+/// It ensures that the callback is only executed once, after the initial build.
+///
+/// The [callback] function is called after the widget has finished building.
+/// It is executed within the [WidgetsBinding.instance.addPostFrameCallback] method, which schedules the callback to be called after the frame has been rendered.
+///
+/// Example usage:
+/// ```dart
+/// usePostBuildCallback(() {
+///   // Perform some action after the widget has finished building
+/// });
+/// ```
 void usePostBuildCallback(VoidCallback callback) {
   final didBuild = useRef(false);
 
