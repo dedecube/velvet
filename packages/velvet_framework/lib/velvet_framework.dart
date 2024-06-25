caa39012c579c2b2f36bb8b28bb8100eb6deb24e
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-25 21:49:49 +0200

    feat: update entrypoint exports

--- packages/velvet_framework/lib/velvet_framework.dart
+++ packages/velvet_framework/lib/velvet_framework.dart
@@ -14,9 +14,9 @@ export 'package:hooks_riverpod/hooks_riverpod.dart'; // internally exports flutt
 /// This is the core library of the framework that contains all the necessary classes and utilities to build a Flutter application.
 
 // @index('./**/exports.dart', f => `export '${f.path}.dart';`)
-export './core/exports.dart';
 export './data_loader/exports.dart';
 export './dev_inspector/exports.dart';
+export './form/exports.dart';
 export './hooks/exports.dart';
 export './http/exports.dart';
 export './kernel/exports.dart';
@@ -26,5 +26,6 @@ export './talker/exports.dart';
 export './theme/exports.dart';
 export './translation/exports.dart';
 export './utils/exports.dart';
+export './validation/exports.dart';
 export './widgets/exports.dart';
 // @endindex
