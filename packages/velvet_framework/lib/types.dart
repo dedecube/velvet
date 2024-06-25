6c834f59429d27cfac6b6d8d60f87bcdd0a8544a
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-06-25 21:49:07 +0200

    feat: ExceptionMatcher now must return a TranslationItem instead of String

--- packages/velvet_framework/lib/types.dart
+++ packages/velvet_framework/lib/types.dart
@@ -1,1 +1,3 @@
-typedef ExceptionMatcher = String? Function(Exception exception);
+import 'package:velvet_framework/translation/translation_item.dart';
+
+typedef ExceptionMatcher = TranslationItem? Function(Exception exception);
