a09fcce3e9ae565cf3eda49be05c7b3a1127f6d7
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-07-03 17:03:14 +0200

    feat: restore config builder

---
+++ packages/velvet_generator/lib/src/generators/_config_value.dart
@@ -0,0 +1,11 @@
+part of 'config_generator.dart';
+
+class ConfigValue {
+  ConfigValue({
+    required this.name,
+    required this.import,
+  });
+
+  final String name;
+  final String import;
+}
