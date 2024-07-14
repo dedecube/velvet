aa4214a2537f6259c2612a6bd24347d59245ba87
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-07-14 16:09:58 +0200

    docs: add docs in RenderableExceptionContract

--- packages/velvet_framework/lib/error_handling/contracts/renderable_exception_contract.dart
+++ packages/velvet_framework/lib/error_handling/contracts/renderable_exception_contract.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+/// The contract for exceptions that can be rendered in the UI.
+///
+/// This contract defines a method `render` that takes a [BuildContext] as a parameter.
+/// Implementing classes should provide an implementation for the `render` method
+/// to display the exception in the user interface.
 abstract class RenderableExceptionContract {
+  /// Renders the exception in the UI.
+  ///
+  /// The [context] parameter is the build context of the widget tree where the exception
+  /// should be rendered. Implementing classes should provide an implementation for this method
+  /// to display the exception in the user interface.
   void render(BuildContext context);
 }
