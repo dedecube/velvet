931959a7c66efdeb79880a7ef123de6cfad0a22e
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-07-15 13:54:26 +0200

    fix: kernel_error_widget now can use translator using kernel resolutionKey

--- packages/velvet_framework/lib/kernel/widgets/kerner_error_widget.dart
+++ packages/velvet_framework/lib/kernel/widgets/kerner_error_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:velvet_framework/translation/extensions/translator_extension.dart';
+import 'package:velvet_framework/velvet_framework.dart';
 
-class KernelErrorWidget extends StatelessWidget {
+class KernelErrorWidget extends HookConsumerWidget {
   const KernelErrorWidget({
     super.key,
     required this.error,
@@ -14,33 +14,56 @@ class KernelErrorWidget extends StatelessWidget {
   final StackTrace stackTrace;
 
   @override
-  MaterialApp build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(context.translate('velvet.kernel.widgets.error.title')),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                context.translate(
-                  'velvet.kernel.widgets.error.message',
-                  args: {
-                    'error': error.toString(),
-                  },
+  build(BuildContext context, WidgetRef ref) {
+    final translator = ref.read(translatorProvider);
+    final lightThemeData = useLightTheme();
+    final darkThemeData = useDarkTheme();
+
+    return StreamBuilder(
+      initialData: translator.currentLocale,
+      stream: translator.localeStream,
+      builder: (context, locale) {
+        return MaterialApp(
+          builder: (context, child) => Builder(
+            key: Kernel.resolutionKey,
+            builder: (context) => Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  context.translate('velvet.kernel.widgets.error.title'),
                 ),
               ),
-              ElevatedButton(
-                onPressed: onRetry,
-                child: Text(
-                  context.translate('velvet.kernel.widgets.error.retry'),
+              body: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      context.translate(
+                        'velvet.kernel.widgets.error.message',
+                        args: {
+                          'error': error.toString(),
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: onRetry,
+                      child: Text(
+                        context.translate('velvet.kernel.widgets.error.retry'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+          theme: lightThemeData,
+          darkTheme: darkThemeData,
+          themeMode: ThemeMode.system,
+          navigatorKey: Kernel.navigatorKey,
+          localizationsDelegates: translator.delegates(),
+          supportedLocales: translator.supportedLocales,
+          locale: locale.data,
+          home: const SizedBox.shrink(),
+        );
+      },
     );
   }
 }
