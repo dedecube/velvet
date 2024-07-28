1d262cf0e19465965cc00ecd585a60049e75f066
Fabrizio Infante <fabrizio@dedecube.com>
2024-07-28 11:21:36 +0200

    feat: removed params in useCaseContract

--- packages/velvet_framework/lib/contracts/use_case_contract.dart
+++ packages/velvet_framework/lib/contracts/use_case_contract.dart
@@ -1,16 +1,17 @@
+import 'dart:async';
+
 /// A contract for defining a use case.
 ///
 /// A use case represents a specific business logic or operation that can be performed in an application.
-/// It typically takes input parameters and returns a result of a specific type.
+/// It returns a result of a specific type.
 /// Implementations of this contract should provide the logic for executing the use case.
 ///
 /// A use case should be a pure function that does not have any side effects.
 /// It should only perform the operation it is designed to do and return the result.
 /// It MUST NOT interact with the UI.
-abstract interface class UseCaseContract<Return, Params> {
-  /// Executes the use case with the given parameters.
+abstract interface class UseCaseContract<Return> {
+  /// Executes the use case.
   ///
-  /// The [params] parameter represents the input parameters required for the use case.
   /// The method should return a [Future] that resolves to the result of the use case.
-  Future<Return> execute(Params params);
+  FutureOr<Return> execute();
 }
