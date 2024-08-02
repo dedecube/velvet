d03d20883158750edf170deaaeb39b3af6727169
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-02 16:35:27 +0200

    feat: add middlewares field in BaseRoute

--- packages/velvet_framework/lib/router/base_route.dart
+++ packages/velvet_framework/lib/router/base_route.dart
@@ -1,6 +1,19 @@
-import 'package:go_router/go_router.dart';
-import 'package:velvet_framework/router/mixins/base_route_page_builder_mixin.dart';
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:velvet_framework/velvet_framework.dart';
 
 class BaseRoute extends GoRouteData with BaseRoutePageBuilderMixin {
   const BaseRoute();
+
+  List<MiddlewareContract> get middlewares => [];
+
+  @override
+  FutureOr<String?> redirect(BuildContext context, GoRouterState state) {
+    if (middlewares.isEmpty) {
+      return null;
+    }
+
+    return MiddlewarePipeline(middlewares).handle(context, state);
+  }
 }
