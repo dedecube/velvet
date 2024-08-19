c34f61109a0b986f3213cc436c3d09c5318c53d6
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-19 17:54:28 +0200

    docs: add doc to some http contracts

--- packages/velvet_framework/lib/http/contracts/http_request_list_contract.dart
+++ packages/velvet_framework/lib/http/contracts/http_request_list_contract.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:velvet_framework/http/contracts/http_request_contract.dart';
 
+/// The contract for a HTTP request that returns a list of items.
+///
+/// For example, this is useful for GET requests that return a list of items.
 abstract class HttpRequestListContract<T>
     extends HttpRequestContract<List<T>, List> {
   T Function(Map<String, dynamic> json) get itemMapper;
--- packages/velvet_framework/lib/http/contracts/http_request_map_contract.dart
+++ packages/velvet_framework/lib/http/contracts/http_request_map_contract.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:velvet_framework/http/contracts/http_request_contract.dart';
 
+/// The contract for a HTTP request that returns a map of key-value pairs.
 abstract class HttpRequestMapContract<T>
     extends HttpRequestContract<T, Map<String, dynamic>> {
   T Function(Map<String, dynamic> json) get itemMapper;
