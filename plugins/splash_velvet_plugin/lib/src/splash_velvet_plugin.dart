374fa1faa659c4f6ed283c36c160c5051e5398da
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-19 17:09:09 +0200

    refactor: SplashVelvetPlugin now is compliante to VelvetPlugin

--- plugins/splash_velvet_plugin/lib/src/splash_velvet_plugin.dart
+++ plugins/splash_velvet_plugin/lib/src/splash_velvet_plugin.dart
@@ -5,22 +5,18 @@ import 'package:velvet_framework/velvet_framework.dart';
 /// A plugin for Velvet Framework that handles the splash screen functionality.
 ///
 /// This plugin handles automatically preserving and removing the native splash.
-/// 
+///
 /// Example of usage:
 /// ```dart
 ///   Kernel()
 ///    ..plugin(SplashVelvetPlugin())
 ///    ..run();
 /// ```
-class SplashVelvetPlugin extends VelvetPluginContract {
+class SplashVelvetPlugin extends VelvetPlugin {
   @override
-  install(Kernel kernel) {
+  void boot() {
     _preserveSplash();
-
-    kernel.boot((ref) {
-      final eventBus = ref.read(eventBusProvider);
-      eventBus.listen<HideLoadingWidgetEvent>((_) => _hideSplash());
-    });
+    listen<HideLoadingWidgetEvent>((_) => _hideSplash());
   }
 
   /// Preserves the native splash screen.
@@ -28,7 +24,7 @@ class SplashVelvetPlugin extends VelvetPluginContract {
   /// This method should be called during the application startup to preserve
   /// the native splash screen. It ensures that the Flutter widgets binding is
   /// initialized before preserving the splash screen.
-  _preserveSplash() {
+  void _preserveSplash() {
     FlutterNativeSplash.preserve(
       widgetsBinding: WidgetsFlutterBinding.ensureInitialized(),
     );
@@ -39,7 +35,7 @@ class SplashVelvetPlugin extends VelvetPluginContract {
   /// This method should be called when the loading process is complete and the
   /// application is ready to display its content. It removes the native splash
   /// screen from the screen.
-  _hideSplash() {
+  void _hideSplash() {
     FlutterNativeSplash.remove();
   }
 }
