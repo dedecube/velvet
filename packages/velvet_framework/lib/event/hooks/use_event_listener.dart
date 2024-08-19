682ac795a722c1ca2e8f653f94442aa8b2ef8c55
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-19 12:16:22 +0200

    feat: useEffectOnce in useEventListener

--- packages/velvet_framework/lib/event/hooks/use_event_listener.dart
+++ packages/velvet_framework/lib/event/hooks/use_event_listener.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:velvet_framework/event/contracts/event_contract.dart';
 import 'package:velvet_framework/event/utils/listen.dart';
+import 'package:velvet_framework/hooks/use_effect_once/use_effect_once.dart';
 
 /// Listens to the specified event and calls the callback when the event is
 /// dispatched.
@@ -9,12 +9,11 @@ import 'package:velvet_framework/event/utils/listen.dart';
 void useEventListener<T extends EventContract>(
   void Function(T event) callback,
 ) {
-  useEffect(
+  useEffectOnce(
     () {
       final subscription = listen<T>(callback);
 
       return subscription.cancel;
     },
-    [],
   );
 }
