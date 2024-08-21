a1fd5d4ad3a5a552fce8bd7c45181e8d7f0b0392
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-21 12:46:47 +0200

    chore: update exports

--- packages/velvet_cli/lib/velvet_cli.dart
+++ packages/velvet_cli/lib/velvet_cli.dart
@@ -1,1 +1,20 @@
 export 'package:dcli/dcli.dart';
+
+// @index(['./src/**/(*.dart)','!./src/**/*.g.dart', '!./src/**/_*.dart'], f => `export '${f.path}.dart';`)
+export './src/commands/list/list_command.dart';
+export './src/commands/make_route/make_route_command.dart';
+export './src/commands/update_cli/update_cli_command.dart';
+export './src/commands/update_framework/update_framework_command.dart';
+export './src/core/container.dart';
+export './src/core/create_velvet_cli.dart';
+export './src/core/velvet_cli.dart';
+export './src/extensions/decode_on_package_resource.dart';
+export './src/extensions/library_uri_on_string.dart';
+export './src/extensions/name_parts_on_string.dart';
+export './src/mixins/import_finder.dart';
+export './src/mixins/part_finder.dart';
+export './src/mixins/writer.dart';
+export './src/services/pubspec.dart';
+export './src/velvet_command_handler.dart';
+export './src/velvet_command.dart';
+// @endindex
