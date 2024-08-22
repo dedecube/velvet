60531d5412f56d4a89a06b628837ef5d7f563655
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-22 16:21:28 +0200

    refactor: error_handling

--- packages/velvet_framework/lib/error_handling/error_handling_plugin.dart
+++ packages/velvet_framework/lib/error_handling/error_handling_plugin.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:velvet_framework/core/plugin/velvet_plugin.dart';
-import 'package:velvet_framework/core/utils.dart';
+import 'package:velvet_framework/core/utils/config.dart';
+import 'package:velvet_framework/core/utils/navigator_key.dart';
 import 'package:velvet_framework/error_handling/config/default_error_handling_config.dart';
 import 'package:velvet_framework/error_handling/contracts/error_handling_config_contract.dart';
 import 'package:velvet_framework/error_handling/renderable_exception.dart';
-import 'package:velvet_framework/kernel/kernel.dart';
 
 class ErrorHandlingPlugin extends VelvetPlugin {
   @override
@@ -20,37 +20,35 @@ class ErrorHandlingPlugin extends VelvetPlugin {
     FlutterError.onError = (FlutterErrorDetails details) {
       final exception = details.exception;
 
-      if (exception is RenderableException) {
-        exception.render(Kernel.navigatorKey.currentState!.context);
-      } else if (exception is Exception) {
-        final errorHandlingConfig = config<ErrorHandlingConfigContract>();
-
-        errorHandlingConfig.renderer(
-          Kernel.navigatorKey.currentState!.context,
-          exception,
-        );
+      if (exception is Exception) {
+        renderException(exception);
       }
 
       FlutterError.presentError(details);
     };
 
     PlatformDispatcher.instance.onError = (exception, stackTrace) {
-      if (exception is RenderableException) {
-        exception.render(Kernel.navigatorKey.currentState!.context);
-
-        return true;
-      } else if (exception is Exception) {
-        final errorHandlingConfig = config<ErrorHandlingConfigContract>();
-
-        errorHandlingConfig.renderer(
-          Kernel.navigatorKey.currentState!.context,
-          exception,
-        );
+      if (exception is Exception) {
+        renderException(exception);
 
         return true;
       }
 
       return false;
     };
+  }
+
+  void renderException(Exception exception) {
+    if (exception is RenderableException) {
+      (exception as RenderableException)
+          .render(navigatorKey().currentState!.context);
+
+      return;
+    }
+
+    config<ErrorHandlingConfigContract>().renderer(
+      navigatorKey().currentState!.context,
+      exception,
+    );
   }
 }
