b468630dd3016ebf5bac6638827a1c3a8f3688fd
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-23 14:23:59 +0200

    feat: add VelvetSubject, VelvetObserver and a contract that works as bridge between event-pattern and observer-pattern

---
+++ packages/velvet_framework/lib/core/event/velvet_observer_from_events.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:velvet_framework/core/event/velvet_observer.dart';
+import 'package:velvet_framework/core/event/velvet_subject.dart';
+
+abstract class VelvetObserverFromEvents<T extends VelvetObserver>
+    extends VelvetSubject<T> {
+  VelvetObserverFromEvents() {
+    bindEvents();
+  }
+
+  @protected
+  void bindEvents();
+}
