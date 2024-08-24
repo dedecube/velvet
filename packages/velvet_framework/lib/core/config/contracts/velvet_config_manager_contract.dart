8807d9c11d2d16703033db9a5bfc24b84b264400
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-24 14:12:18 +0200

    feat: add refresh and all in VelvetConfigManager

--- packages/velvet_framework/lib/core/config/contracts/velvet_config_manager_contract.dart
+++ packages/velvet_framework/lib/core/config/contracts/velvet_config_manager_contract.dart
@@ -3,4 +3,6 @@ import 'package:velvet_framework/core/config/velvet_config.dart';
 abstract class VelvetConfigManagerContract {
   T register<T extends VelvetConfig>(T config);
   T get<T extends VelvetConfig>();
+  List<VelvetConfig> all();
+  void refresh();
 }
