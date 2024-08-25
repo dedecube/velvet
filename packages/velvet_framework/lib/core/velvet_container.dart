ba967d5d82b9353bd8f4060c512123cd8f43ce74
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-25 16:54:37 +0200

    fix: container is now a VelvetContainer instance

--- packages/velvet_framework/lib/core/velvet_container.dart
+++ packages/velvet_framework/lib/core/velvet_container.dart
@@ -1,9 +1,9 @@
-import 'package:get_it/get_it.dart';
-
-typedef VelvetContainer = GetIt;
+import 'package:velvet_framework/core/container/velvet_container.dart';
 
 /// The global container for the Velvet framework.
 ///
 /// This container is used to register and resolve dependencies.
-/// Behind the scenes, it uses the `get_it` package.
-final VelvetContainer container = GetIt.instance;
+///
+/// Behind the scenes, it uses the `get_it` package
+/// and operates mostly as a proxy to the `GetIt` instance.
+final container = VelvetContainer();
