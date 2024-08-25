e929f90b61f821b46d558932c96d7ec57977fa50
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-25 17:01:55 +0200

    feat: update plugin loader

--- packages/velvet_framework/lib/kernel/loaders/velvet_plugin_loader.velvet.dart
+++ packages/velvet_framework/lib/kernel/loaders/velvet_plugin_loader.velvet.dart
@@ -5,17 +5,20 @@
 // **************************************************************************
 
 import 'package:velvet_framework/form/form_plugin.dart';
-import 'package:velvet_framework/core/plugin/anonymous_velvet_plugin.dart';
+import 'package:velvet_framework/core/env/env_plugin.dart';
 import 'package:velvet_framework/translation/translation_plugin.dart';
 import 'package:velvet_framework/error_handling/error_handling_plugin.dart';
 import 'package:velvet_framework/theme/theme_plugin.dart';
+import 'package:velvet_framework/store/store_plugin.dart';
 import 'package:velvet_framework/router/router_plugin.dart';
 import 'package:velvet_framework/core/plugin/velvet_plugin.dart';
 
 List<VelvetPlugin> $velvetPluginLoaderItems = [
   FormPlugin(),
+  EnvPlugin(),
   TranslationPlugin(),
   ErrorHandlingPlugin(),
   ThemePlugin(),
+  StorePlugin(),
   RouterPlugin()
 ];
