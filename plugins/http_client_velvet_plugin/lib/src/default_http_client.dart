2c9dc61b5e8d0ccf35e1454cd2c401e8130357fc
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-10-29 13:36:18 +0100

    feat: HttpClientVelvetPlugin now registrer a singleton http client and check for HTTP_CLIENT_BASE_URL environment variable

--- plugins/http_client_velvet_plugin/lib/src/default_http_client.dart
+++
@@ -1,5 +0,0 @@
-import 'package:http_client_velvet_plugin/src/contracts/http_client_contract.dart';
-
-class DefaultHttpClient extends HttpClientContract {
-  DefaultHttpClient(super.config);
-}
--- plugins/http_client_velvet_plugin/lib/src/http_client_velvet_plugin.dart
+++ plugins/http_client_velvet_plugin/lib/src/http_client_velvet_plugin.dart
@@ -1,16 +1,49 @@
 import 'package:http_client_velvet_plugin/http_client_velvet_plugin.dart';
-import 'package:http_client_velvet_plugin/src/contracts/http_client_contract.dart';
-import 'package:http_client_velvet_plugin/src/default_http_client.dart';
+import 'package:http_client_velvet_plugin/src/config/default_http_client_config.dart';
 import 'package:velvet_framework/velvet_framework.dart';
 
 class HttpClientVelvetPlugin extends VelvetPlugin {
+  static const String velvetHttpToken = 'http_client.velvet_http';
+
+  static const String velvetHttpConfigToken = 'http_client.velvet_http_config';
+
   @override
   void register() {
-    container.registerLazySingleton<HttpClientContract>(() {
-      final httpClientConfig = config<HttpClientConfigContract>();
-      final httpConfig = HttpConfig(baseURL: httpClientConfig.baseURL);
+    configManager().register<HttpClientConfigContract>(
+      DefaultHttpClientConfig(),
+    );
 
-      return DefaultHttpClient(httpConfig);
-    });
+    container.registerLazySingleton<VelvetHttpConfig>(
+      () {
+        final apiConfig = config<HttpClientConfigContract>();
+
+        return VelvetHttpConfig(baseURL: apiConfig.baseURL);
+      },
+      instanceName: velvetHttpConfigToken,
+    );
+
+    container.registerLazySingleton<VelvetHttp>(
+      () {
+        final apiConfig = config<HttpClientConfigContract>();
+
+        final httpConfig = VelvetHttpConfig(baseURL: apiConfig.baseURL);
+
+        return VelvetHttp(httpConfig);
+      },
+      instanceName: velvetHttpToken,
+    );
+  }
+
+  @override
+  void boot() {
+    _checkBaseURL();
+  }
+
+  void _checkBaseURL() {
+    if (container.httpClientConfig.baseURL.isEmpty) {
+      throw Exception(
+        'HTTP_CLIENT_BASE_URL is not set. Please set it in .env or provide a custom implementation of HttpClientConfigContract',
+      );
+    }
   }
 }
