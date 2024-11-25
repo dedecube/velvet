b86e61ac88e440058c1e67516156116a8b6e7a96
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-11-25 18:20:02 +0100

    chore: update exports

--- plugins/connectivity_velvet_plugin/lib/connectivity_velvet_plugin.dart
+++ plugins/connectivity_velvet_plugin/lib/connectivity_velvet_plugin.dart
@@ -7,4 +7,7 @@ export './src/events/online_event.dart';
 export './src/hooks/use_is_online.dart';
 export './src/hooks/use_on_offline.dart';
 export './src/hooks/use_on_online.dart';
+export './src/notifiers/connectivity_notifier.dart';
+export './src/utils/is_offline.dart';
+export './src/utils/is_online.dart';
 // @endindex
