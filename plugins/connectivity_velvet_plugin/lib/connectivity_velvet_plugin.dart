364195f8c0739f8dc74ec763f32e5457a048fac8
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-11-25 11:32:06 +0100

    feat: add sync with notifier in ConnectivityVelvetPlugin

--- plugins/connectivity_velvet_plugin/lib/connectivity_velvet_plugin.dart
+++ plugins/connectivity_velvet_plugin/lib/connectivity_velvet_plugin.dart
@@ -4,7 +4,7 @@ library connectivity_velvet_plugin;
 export './src/connectivity_velvet_plugin.dart';
 export './src/events/offline_event.dart';
 export './src/events/online_event.dart';
+export './src/hooks/use_is_online.dart';
 export './src/hooks/use_on_offline.dart';
 export './src/hooks/use_on_online.dart';
-export './src/notifiers/connectivity_notifier.dart';
 // @endindex
--- plugins/connectivity_velvet_plugin/lib/src/connectivity_velvet_plugin.dart
+++ plugins/connectivity_velvet_plugin/lib/src/connectivity_velvet_plugin.dart
@@ -3,15 +3,26 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:connectivity_velvet_plugin/src/events/offline_event.dart';
 import 'package:connectivity_velvet_plugin/src/events/online_event.dart';
+import 'package:connectivity_velvet_plugin/src/notifiers/connectivity_notifier.dart';
+import 'package:velvet_annotation/velvet_annotation.dart';
 import 'package:velvet_framework/velvet_framework.dart';
 
 class ConnectivityVelvetPlugin extends VelvetPlugin {
   @override
-  FutureOr<void> boot() {
-    Connectivity().onConnectivityChanged.listen(_onConnectivityChanged);
+  FutureOr<void> register() {
+    _syncNotifier();
+    _watchConnectivityChanges();
   }
 
-  void _onConnectivityChanged(List<ConnectivityResult> onData) async {
+  @override
+  FutureOr<void> boot() {
+    // Enforce the initial state
+    _onConnectivityChanged();
+  }
+
+  /// Handle the connectivity change event
+  /// and dispatch the appropriate event
+  void _onConnectivityChanged() async {
     final result = await Connectivity().checkConnectivity();
 
     if (result.contains(ConnectivityResult.none)) {
@@ -19,5 +30,24 @@ class ConnectivityVelvetPlugin extends VelvetPlugin {
     } else {
       event(OnlineEvent());
     }
+  }
+
+  /// Sync the notifier with the current connectivity state
+  void _syncNotifier() {
+    listen<OnlineEvent>((_) {
+      riverpodContainer().read(connectivityNotifierProvider.notifier).online();
+    });
+
+    listen<OfflineEvent>((_) {
+      riverpodContainer().read(connectivityNotifierProvider.notifier).offline();
+    });
+  }
+
+  /// Watch for connectivity changes
+  /// and dispatch the appropriate event
+  void _watchConnectivityChanges() {
+    Connectivity()
+        .onConnectivityChanged
+        .listen((_) => _onConnectivityChanged());
   }
 }
